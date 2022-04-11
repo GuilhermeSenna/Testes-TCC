19fc7673eea16201b022bb686abe023c7aa3fad1
zBiggui <bigguisenna@gmail.com>
2022-04-10 22:46:16 -0300

    Mais comentários

--- GLCM classification.py
+++ GLCM classification.py
@@ -1,14 +1,3 @@
-# https://youtu.be/5x-CIHRmMNY
-"""
-@author: Sreenivas Bhattiprolu
-skimage.feature.greycomatrix(image, distances, angles, levels=None, symmetric=False, normed=False)
-distances - List of pixel pair distance offsets.
-angles - List of pixel pair angles in radians.
-skimage.feature.greycoprops(P, prop)
-prop: The property of the GLCM to compute.
-{‘contrast’, ‘dissimilarity’, ‘homogeneity’, ‘energy’, ‘correlation’, ‘ASM’}
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
@@ -26,11 +15,10 @@ from skimage.measure import shannon_entropy
 # print(os.listdir("images/natural/"))
 
 # Resize images to
-# SIZE = 128
+SIZE = 256
 
 # Numpy Array (explicação)
 # The main benefits of using NumPy arrays should be smaller memory consumption and better runtime behavior.
-
 
 def train_set():
     # Imagens de treino e suas classes
@@ -42,7 +30,7 @@ def train_set():
         label = directory_path.split("\\")[-1]
         for img_path in glob.glob(os.path.join(directory_path, "*.jpg")):
             img = cv2.imread(img_path, 0)  # Reading color images
-            img = cv2.resize(img, (256, 256))  # Resize images
+            img = cv2.resize(img, (SIZE, SIZE))  # Resize images
             train_images.append(img)
             train_labels.append(label)
 
@@ -60,7 +48,7 @@ def test_set():
         fruit_label = directory_path.split("\\")[-1]
         for img_path in glob.glob(os.path.join(directory_path, "*.jpg")):
             img = cv2.imread(img_path, 0)
-            img = cv2.resize(img, (256, 256))  # Resize images
+            img = cv2.resize(img, (SIZE, SIZE))  # Resize images
             test_images.append(img)
             test_labels.append(fruit_label)
 
@@ -99,10 +87,10 @@ def feature_extractor(dataset):
         # pi/2 - 90º
         configs = [
             {'distancia': [1], 'angulo': [0]},
-            {'distancia': [2], 'angulo': [0]},
             {'distancia': [3], 'angulo': [0]},
-            {'distancia': [1], 'angulo': [np.pi / 4]},
-            {'distancia': [1], 'angulo': [np.pi / 2]},
+            {'distancia': [5], 'angulo': [0]},
+            {'distancia': [0], 'angulo': [np.pi / 4]},
+            {'distancia': [0], 'angulo': [np.pi / 2]},
         ]
 
         # Adicionando dados ao Dataframe
@@ -141,16 +129,20 @@ test_images, test_labels = test_set()
 # x_test: Imagens de teste
 # y_test: Classes de teste
 
+# for train in train_labels:
+#     print(train)
+
 x_train, x_test = train_images, test_images
 
 # Instância do codificador
 le = preprocessing.LabelEncoder()
-y_train, y_test = pre_processing(le, train_labels, test_labels)  # Classes para inteiros (0 ... nº classes - 1)
+y_train, y_test = pre_processing(le, train_labels, test_labels)  # Codificando nome das Classes para inteiros (0 ... nº classes - 1)
 
 # Normalize pixel values to between 0 and 1
 # x_train, x_test = x_train / 255.0, x_test / 255.0
 
 ####################################################################
+
 # Extração de atributos das imagens de treino
 image_features = feature_extractor(x_train)
 X_for_ML = image_features
@@ -177,35 +169,49 @@ RF_model.fit(X_for_ML, y_train) #For sklearn no one hot encoding
 import lightgbm as lgb
 
 # Class names for LGBM start at 0 so reassigning labels from 1,2,3,4 to 0,1,2,3
+
+# Montagem do Dataset
+# X_for_ML: Imagens de treino quantificadas pelos atributos de imagem
+# y_train: Labels das imagens de treino
 d_train = lgb.Dataset(X_for_ML, label=y_train)
+
+print(d_train)
 
 # https://lightgbm.readthedocs.io/en/latest/Parameters.html
 
-lgbm_params = {'learning_rate': 0.05,
-               'boosting_type': 'goss',
-               'objective': 'multiclass',
+lgbm_params = {
+                'learning_rate': 0.05,         # Taxa de aprendizagem
+               'boosting_type': 'goss',        # Boosting utilizado
+               'objective': 'multiclass',      # Multiclasse
                'metric': 'multi_logloss',
-               'num_leaves': 100,
-               'max_depth': 10,
-               'num_class': 4}
+               'num_leaves': 100,              # Número máximos de folhas
+               'max_depth': 10,                # Máxima profundidade da árvore
+               'num_class': len(set(y_train))  #  Nº de classes utilizadas - Obtém dinamicamente com base nos valores distintos da lista
+               }
 
-lgb_model = lgb.train(lgbm_params, d_train, 100)  # 50 iterations. Increase iterations for small learning rates
+# Treino do modelo com os parâmetros definidos
+# lgbm_params: Parâmetros do treino
+# d_train: Dataset montantado após o cálculo dos atributos
+# Quantidade de iterações
+lgb_model = lgb.train(lgbm_params, d_train, 100)
 
-# Predict on Test data
-# Extract features from test data and reshape, just like training data
+# Predição nos dados de teste
+# Extração de atributos do conjunto de teste e reshape igual o conjunto de treino
 test_features = feature_extractor(x_test)
 test_features = np.expand_dims(test_features, axis=0)
 test_for_RF = np.reshape(test_features, (x_test.shape[0], -1))
 
-# Predict on test
+# Predição nos testes
 test_prediction = lgb_model.predict(test_for_RF)
 test_prediction = np.argmax(test_prediction, axis=1)
-# Inverse le transform to get original label back.
+
+# Inversão da codificação dos labels para obter as informaçoes originais. Inteiros (0 ... nº classes - 1) para os nomes das classes
 test_prediction = le.inverse_transform(test_prediction)
 
 # Print overall accuracy
 from sklearn import metrics
 
+# Acuracidade
 print("Accuracy = ", metrics.accuracy_score(test_labels, test_prediction))
 
 # Print confusion matrix
@@ -213,29 +219,30 @@ from sklearn.metrics import confusion_matrix
 
 cm = confusion_matrix(test_labels, test_prediction)
 
-# print(train_labels)
-
 fig, ax = plt.subplots(figsize=(6, 6))  # Sample figsize in inches
 sns.set(font_scale=1.6)
 sns.heatmap(cm, annot=True, linewidths=.5, ax=ax)
 
-# Check results on a few random images
+
+# Checando resultados para imagens aleatórias
 import random
 
 for c in range(10):
-    n = random.randint(0, x_test.shape[0] - 1)  # Select the index of image to be loaded for testing
+    n = random.randint(0, x_test.shape[0] - 1)  # Randomizando o index da imagem
     img = x_test[n]
     plt.imshow(img)
     plt.show()
 
-    # Extract features and reshape to right dimensions
-    input_img = np.expand_dims(img, axis=0)  # Expand dims so the input is (num images, x, y, c)
+    # Extraindo atributos e reshaping
+    input_img = np.expand_dims(img, axis=0)  # Expandindo as dimensões para ser (num images, x, y, c)
     input_img_features = feature_extractor(input_img)
     input_img_features = np.expand_dims(input_img_features, axis=0)
     input_img_for_RF = np.reshape(input_img_features, (input_img.shape[0], -1))
-    # Predict
+
+
+    # Predição para as imagens aleatórias
     img_prediction = lgb_model.predict(input_img_for_RF)
     img_prediction = np.argmax(img_prediction, axis=1)
-    img_prediction = le.inverse_transform([img_prediction])  # Reverse the label encoder to original name
+    img_prediction = le.inverse_transform([img_prediction])  # Revertendo para os labels originais
     print("The prediction for this image is: ", img_prediction)
     print("The actual label for this image is: ", test_labels[n])
